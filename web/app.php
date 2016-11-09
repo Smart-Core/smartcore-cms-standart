771a339541a90e0996f23f2e20f8f8662c11aa84
Smart-Core <artem.digi@gmail.com>
2016-11-09 17:55:25 +0700

    fixes

--- web/app.php
+++ web/app.php
@@ -4,9 +4,9 @@ define('APPKERNEL_DEBUG', true);
 
 use Symfony\Component\HttpFoundation\Request;
 
+include_once __DIR__.'/../var/bootstrap.php.cache';
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
 
 \Profiler::enable();
 
