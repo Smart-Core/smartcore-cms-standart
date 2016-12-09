45f4561d5021a71c2d939286b6fe64f8f52e01b6
Smart-Core <artem.digi@gmail.com>
2016-12-09 16:50:39 +0700

    update to symfony 3.2

--- web/app.php
+++ web/app.php
@@ -4,7 +4,10 @@ define('APPKERNEL_DEBUG', true);
 
 use Symfony\Component\HttpFoundation\Request;
 
-include_once __DIR__.'/../var/bootstrap.php.cache';
+if (APPKERNEL_DEBUG === false) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 
